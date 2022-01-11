36f6443cd590a134a7d1289913850360a85b4ac7
be1an <8497715@gmail.com>
2022-01-11 23:16:46 +0500

    fix app js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import s from "./Header.module.css";
 
 export class Header extends Component {
   render() {
     return (
-      <header className="App-header">
+      <header className={s.appHeader}>
         <div>CRA TO DO</div>
       </header>
     );
