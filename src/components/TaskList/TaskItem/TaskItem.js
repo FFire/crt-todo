056d7d2d5ddf139eb46ca10a1fe8ec3106fcd10f
be1an <8497715@gmail.com>
2022-01-16 02:14:06 +0500

    fix: console.log removed

--- src/components/TaskList/TaskItem/TaskItem.js
+++ src/components/TaskList/TaskItem/TaskItem.js
@@ -1,19 +1,15 @@
 import classNames from 'classnames';
 import React, { Component } from 'react';
-import { ThemeContext, themes } from './../../App/themeContext';
+import { ThemeContext, themes } from '../../App/themeContext';
 import s from './TaskItem.module.css';
 
 export class TaskItem extends Component {
   static contextType = ThemeContext;
 
-  componentDidMount(){
-    console.log('mount', this.context);
-  }
-
   render() {
     const { id, text, isDone } = this.props.task;
     const UiTheme = this.context;
-    const itemClass = classNames(s.item, { [s.dark]: UiTheme === themes.dark,  [s.done]: isDone });
+    const itemClass = classNames(s.item, { [s.dark]: UiTheme === themes.dark, [s.done]: isDone });
     return (
       <li>
         <input
