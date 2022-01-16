3864f0ad224f5c3715615ce000e9157f2feff793
be1an <8497715@gmail.com>
2022-01-16 23:23:23 +0500

    fix: by PR comments

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -7,16 +7,12 @@ export class Spinner extends React.Component {
   static contextType = ThemeContext;
 
   render() {
-    const { isLoading } = this.props;
     const UiTheme = this.context;
-    const cssClasses = classNames(s.spinner, { [s.dark]: UiTheme === theme.dark });
-    if (isLoading) {
-      return (
-        <p className={cssClasses}>
-          Data is loading...
-        </p>
-      );
-    }
-    return null;
+    const cssClasses = classNames(s.spinner, { [s.dark]: UiTheme === theme.DARK });
+    return (
+      <p className={cssClasses}>
+        Data is loading...
+      </p>
+    );
   }
 }
