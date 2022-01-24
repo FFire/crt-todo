5863acac0fbe9c50aa66a56b159a47126feb6ef2
be1an <8497715@gmail.com>
2022-01-24 23:22:33 +0500

    before filter update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -9,13 +9,13 @@ export const Filter = (props) => {
   } = props;
   const filterList = stateFilterNames.map((filterName) => (
     <span key={filterName}>
-        <input type='radio' id={`filter-${filterName}`}
-               name='show' value={filterName}
-               checked={stateFilter === filterName}
-               onChange={handleStateFilter}
-        />
-        <label htmlFor={`filter-${filterName}`}>{filterName}</label>
-      </span>));
+      <input type='radio' id={`filter-${filterName}`}
+        name='show' value={filterName}
+        checked={stateFilter === filterName}
+        onChange={handleStateFilter}
+      />
+      <label htmlFor={`filter-${filterName}`}>{filterName}</label>
+    </span>));
 
   return (
     <div className={s.filter}>
@@ -37,6 +37,29 @@ export const Filter = (props) => {
         onClick={handleDeleteCompleted}
       >Delete completed
       </button>
+      <div class="flex bg-slate-100 text-sm font-light rounded-sm w-full">
+        <div>
+          <input type="radio" name="option" id="1" className="peer" checked hidden />
+          <label for="1"
+            className="block text-center border border-slate-700 rounded-l-md px-5 py-1 cursor-pointer select-none
+                peer-checked:bg-slate-300">All</label>
+        </div>
+
+        <div>
+          <input type="radio" name="option" id="2" className="peer" hidden />
+          <label for="2"
+            className="block text-center  border border-slate-700 px-5 py-1 cursor-pointer select-none
+            peer-checked:bg-slate-300">Active</label>
+        </div>
+
+        <div>
+          <input type="radio" name="option" id="3" className="peer" hidden />
+          <label for="3"
+            className="block text-center  border border-slate-700 rounded-r-md px-5 py-1 cursor-pointer select-none
+            peer-checked:bg-slate-300">Completed</label>
+        </div>
+
+      </div>
     </div>
   );
 };
--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -5,23 +5,25 @@ import '../../styles/output.css';
 
 export const ThemeToggle = ({ uiTheme, handleThemeToggle }) => (
   <>
-    <div className='w-full pl-6 py-1 bg-slate-100 flex items-center justify-between hover:bg-slate-200'>
-      <div className='flex items-center'>
-        <input
-          className='flex-none h-4 w-4 accent-slate-600 hover:scale-110 hover:accent-slate-500'
-          type='checkbox'
-          id='ThemeToggle'
-          defaultChecked={uiTheme === theme.DARK}
-          onChange={handleThemeToggle}
-        />
-        <label
-          className='mx-2 text-gray-900/30 hover:text-gray-900/70'
-          htmlFor='ThemeToggle'
-        >Dark mode</label>
-      </div>
-      <div className='pr-10 space-x-4 '>
-        <a className='text-gray-900/30 hover:text-gray-900/70' href="#">Home</a>
-        <a className='text-gray-900/30 hover:text-gray-900/70' href='#'>About</a>
+    <div className=' bg-slate-100'>
+      <div className='w-full pl-6 py-1 bg-slate-100 flex items-center justify-between rounded-lg hover:bg-slate-200'>
+        <div className='flex items-center'>
+          <input
+            className='flex-none h-4 w-4 accent-slate-600 hover:scale-110 hover:accent-slate-500'
+            type='checkbox'
+            id='ThemeToggle'
+            defaultChecked={uiTheme === theme.DARK}
+            onChange={handleThemeToggle}
+          />
+          <label
+            className='mx-2 text-gray-900/30 hover:text-gray-900/70'
+            htmlFor='ThemeToggle'
+          >Dark mode</label>
+        </div>
+        <div className='pr-10 space-x-4 '>
+          <a className='text-gray-900/30 hover:text-gray-900/70' href="#">Home</a>
+          <a className='text-gray-900/30 hover:text-gray-900/70' href='#'>About</a>
+        </div>
       </div>
     </div>
   </>
