c34eb92fd7f1c0ef7c3fc6405627883646c40ea6
be1an <8497715@gmail.com>
2022-01-26 11:37:35 +0500

    fix tailwind integration

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,17 @@ import React from 'react';
 import '../../styles/output.css';
 
 export const Header = () => (
-  <header className='bg-slate-100 rounded-t-lg'>
-    <div className='p-10 text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-l
-      from-emerald-500 to-fuchsia-500 text-center'>CRA TO DO</div>
+  <header className='bg-slate-100 sm:rounded-t-lg'>
+    <div className='
+      text-transparent bg-clip-text bg-gradient-to-l from-emerald-500 to-fuchsia-500 text-center
+      p-4 text-3xl font-semibold
+      sm:p-4 sm:text-3xl sm:font-bold
+      md:p-10 md:text-4xl md:font-extrabold
+      lg:p-10 lg:text-5xl
+      xl:p-10 xl:text-6xl
+      '>CRA TO DO
+    </div>
   </header>
 );
+
+// { /* sm 640, md 758, lg 1024, xl 1280, 2xl 1536 */ }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ import './styles/output.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className='min-h-screen bg-gray-50 py-6 flex flex-col relative overflow-hidden sm:py-12'>
-      <section className='transition-all relative w-2/3 px-2 pt-10 pb-8 bg-white shadow-xl ring-2 ring-gray-900/5 sm:max-w-4xl sm:mx-auto sm:rounded-lg sm:px-10'>
+    <div className='min-h-screen bg-gray-50  flex flex-col relative overflow-hidden'>
+      <section className='
+        transition-all relative bg-white shadow-xl ring-2 ring-gray-900/5
+        mx-auto px-0 pt-0 pb-0 min-w-full
+        sm:mx-auto sm:rounded-lg sm:p-6 sm:w-full sm:min-w-0
+        md:max-w-2xl md:mx-auto md:rounded-lg md:px-10 md:w-11/12 md:pt-10 md:pb-8
+        lg:max-w-3xl lg:mx-auto lg:rounded-lg lg:px-10 lg:w-5/6 lg:pt-10 lg:pb-8
+        xl:max-w-4xl xl:mx-auto xl:rounded-lg xl:px-10 xl:w-3/4 xl:pt-10 xl:pb-8
+        2xl:max-w-4xl 2xl:mx-auto 2xl:rounded-lg 2xl:px-10 2xl:w-2/3 2xl:pt-10 2xl:pb-8
+      '>
         <App />
       </section>
     </div>
