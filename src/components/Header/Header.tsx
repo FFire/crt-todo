f82cc13755b234f147c93e8d8dd7acd0cc5d2d8a
be1an <8497715@gmail.com>
2022-02-06 16:05:39 +0500

    tailwind initial integrate

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react';
 import React, { useContext } from 'react';
 import StoreContext from '../../store/StoreContext';
-import s from './Header.module.css';
+import '../../styles/output.css';
 
 export interface IHeaderMessage {
   text: string
@@ -11,9 +11,18 @@ const Header = (): JSX.Element => {
   const { rootStore } = useContext(StoreContext);
 
   return (
-        <header className={s.appHeader}>
-          <div>{rootStore.appCaption}</div>
-        </header>
+    <header className='bg-slate-100 sm:rounded-t-lg'>
+      <div className='
+        text-transparent bg-clip-text bg-gradient-to-l duration-500 from-emerald-500 to-fuchsia-500 text-center
+        p-4 text-3xl font-semibold
+        sm:p-4 sm:text-3xl sm:font-bold
+        md:p-10 md:text-4xl md:font-extrabold
+        lg:p-10 lg:text-5xl
+        xl:p-10 xl:text-6xl
+      '>
+        {rootStore.appCaption}
+      </div>
+    </header>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,20 @@ import './index.css';
 import { AboutPage, MainPage, NotFoundPage } from './pages/pages';
 import { routePaths } from './routes/routes';
 import StoreContext, { stores } from './store/StoreContext';
+import './styles/output.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <main className='App'>
-      <section className='todoapp'>
+    <main className='min-h-screen bg-gray-50  flex flex-col relative overflow-hidden'>
+      <section className='
+          transition-all duration-500 bg-white shadow-xl ring-2 ring-gray-900/5
+          mx-auto px-0 pt-0 pb-0 min-w-full
+          sm:max-w-xl sm:rounded-lg sm:p-3 sm:w-full sm:min-w-0
+          md:max-w-2xl md:mx-auto md:rounded-lg md:p-10 md:w-11/12 md:my-3
+          lg:max-w-3xl lg:mx-auto lg:rounded-lg lg:w-5/6 lg:px-12
+          xl:max-w-4xl xl:mx-auto xl:rounded-lg xl:w-3/4 xl:px-20
+          2xl:max-w-4xl 2xl:mx-auto 2xl:rounded-lg 2xl:w-2/3
+        '>
         <BrowserRouter>
           <StoreContext.Provider value={stores}>
           <Header />
